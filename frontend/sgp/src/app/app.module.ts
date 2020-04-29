b7da27eddc107ab5249d97413f52ca25176c318e
Arthur Neitzel <arthneitzel@gmail.com>
2020-04-29 16:57:51 -0300

    Criação inicial do card

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
